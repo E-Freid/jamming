ebab54ee9bbb0f5e9bcb2048a5ee04ea7339ed36
Eli Freid <eli.freid@gmail.com>
2021-10-07 17:30:35 +0300

    asd

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,12 +2,26 @@ import React from 'react';
 import './SearchBar.css';
 
 export class SearchBar extends React.Component{
+    constructor(props){
+        super(props);
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+
     render(){
         return (
             <div class="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" />
+                <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist" />
                 <button className="SearchButton">SEARCH</button>
             </div>
         )
+    }
+
+    search(){
+        this.props.onSearch(this.state.term);
+    }
+
+    handleTermChange(e){
+        this.setState({term: e.target.value});
     }
 }
